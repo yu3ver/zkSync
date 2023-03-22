6c957bb3a5601a058a0cbb577868c77a9f121016
y3v <alex@yuzver.name>
2023-03-22 21:57:50 +0200

    Create hardhat.config.ts

---
+++ hardhat.config.ts
@@ -0,0 +1,21 @@
+import "@matterlabs/hardhat-zksync-deploy";
+import "@matterlabs/hardhat-zksync-solc";
+
+module.exports = {
+  zksolc: {
+    version: "1.3.1",
+    compilerSource: "binary",
+    settings: {},
+  },
+  defaultNetwork: "zkTestnet",
+  networks: {
+    zkTestnet: {
+      url: "https://zksync2-testnet.zksync.dev", // URL of the zkSync network RPC
+      ethNetwork: "goerli", // Can also be the RPC URL of the Ethereum network (e.g. https://goerli.infura.io/v3/<API_KEY>)
+      zksync: true,
+    },
+  },
+  solidity: {
+    version: "0.8.17",
+  },
+};
